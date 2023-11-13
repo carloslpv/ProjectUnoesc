ac990dbbbd38cca04ff314d5df44f65440574a74
Carlos <carloscelpv@outlook.com>
2023-11-12 22:00:31 -0300

    Upload de arquivos para repositorio git

---
+++ ProjectUnoesc/src/test/java/br/edu/unoesc/desafiofullstack/DesafioFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.desafiofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
