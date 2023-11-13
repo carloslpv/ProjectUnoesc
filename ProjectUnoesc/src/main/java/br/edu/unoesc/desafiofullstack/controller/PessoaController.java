615e5c5e4e086001a35827e438fd2e4661c14379
carloslpv <carloscelpv@outlook.com>
2023-11-13 14:16:52 -0300

    Realizada comunicação com JSON externo para o endereco "/pessoa"

---
+++ ProjectUnoesc/src/main/java/br/edu/unoesc/desafiofullstack/controller/PessoaController.java
@@ -0,0 +1,26 @@
+package br.edu.unoesc.desafiofullstack.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Import no pacote de entidades para usar classe pessoa
+import br.edu.unoesc.desafiofullstack.entities.Pessoa;
+
+/*
+ * Definição de um controller para a url /pessoa,
+ *Ou seja, chegou uma requisição para /pessoa, o spring chamará este controller
+ */
+@RestController
+@RequestMapping("pessoa") 
+public class PessoaController {
+
+	//Método para cadastrar pessoa, se chegar uma requisição do tipo Post, deverá chamar o método cadastrarPessoa
+	@PostMapping
+	//Utilizando o padrão DTO - Data Transfer Object utilizado para representar os dados que chegam e saem da API
+	public void cadastroPessoa(@RequestBody Pessoa pessoajson) { //neste caso estou dizendo para a aplicação que deve pegar o corpo da requisição
+		System.out.println(pessoajson);
+	}
+	
+}
