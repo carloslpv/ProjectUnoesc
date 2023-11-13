b8114673b6ecc55163142d232c6dbe7f1bc91269
carloslpv <carloscelpv@outlook.com>
2023-11-13 14:08:53 -0300

    Ajuste de classes do pacote entities para record

--- ProjectUnoesc/src/main/java/br/edu/unoesc/desafiofullstack/entities/Contato.java
+++ ProjectUnoesc/src/main/java/br/edu/unoesc/desafiofullstack/entities/Contato.java
@@ -1,50 +1,6 @@
 package br.edu.unoesc.desafiofullstack.entities;
 
-public class Contato {
-	
-	//Declaração dos atributos da classe pessoa
-	private Long codigo;
-	private String telefone;
-	private String email;
-	//Declaração do atributo pessoa da classe pessoa
-	private Pessoa pessoa;
-	
-	//Construtor declarado como vazio evidenciando que a classe pode ser instanciada sem nenhum argumento
-	public Contato() {
-	}
-
-	//Getters e Setters para acesso das informações
-	public Long getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(Long codigo) {
-		this.codigo = codigo;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Pessoa getPessoa() {
-		return pessoa;
-	}
-
-	public void setPessoa(Pessoa pessoa) {
-		this.pessoa = pessoa;
-	}
-	
-	
+//Record seria como se fosse uma classe mas com os dados imutáveis
+//Declaração da record Contato com os dados conforme documentação GIT
+public record Contato (Long codigo, String telefone, String email, Pessoa pessoa){
 }
--- ProjectUnoesc/src/main/java/br/edu/unoesc/desafiofullstack/entities/Pessoa.java
+++ ProjectUnoesc/src/main/java/br/edu/unoesc/desafiofullstack/entities/Pessoa.java
@@ -2,60 +2,7 @@ package br.edu.unoesc.desafiofullstack.entities;
 
 import java.sql.Date;
 
-public class Pessoa {
-	
-	//Declaração dos atributos da classe pessoa
-	private Long codigo;
-	private String nome;
-	private String cpf;
-	private String sexo;
-	private Date dataNascimento;
-	
-	//Construtor declarado como vazio evidenciando que a classe pode ser instanciada sem nenhum argumento
-	public Pessoa() {
-	}
-
-	//Getters e Setters para acesso das informações
-	public Long getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(Long codigo) {
-		this.codigo = codigo;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getSexo() {
-		return sexo;
-	}
-
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
-
-	public Date getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(Date dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	
-	
+//Record seria como se fosse uma classe mas com os dados imutáveis
+//Declaração da record Pessoa com os dados conforme documentação GIT
+public record Pessoa (Long codigo, String nome, String cpf, String sexo, Date dataNascimento){
 }
